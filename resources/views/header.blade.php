7c3a86c935e096d299ec4e30e2b0e5b19a05b4b8
Bagus Andre Wijaya <bagusandrewijaya19@gmail.com>
2023-06-18 13:40:51 +0700

    first commit

---
+++ resources/views/header.blade.php
@@ -0,0 +1,196 @@
+
+<header class="header">
+    <div class="header__inner">
+    
+    <div class="header__brand">
+    <div class="brand-wrap">
+    
+    <a href="index.html" class="brand-img stretched-link">
+    <img src="assets/img/logo.svg" alt="Nifty Logo" class="Nifty logo" width="40" height="40">
+    </a>
+    
+    <div class="brand-title">Nifty</div>
+    
+    </div>
+    </div>
+    
+    <div class="header__content">
+    
+    <div class="header__content-start">
+    
+    <button type="button" class="nav-toggler header__btn btn btn-icon btn-sm" aria-label="Nav Toggler">
+    <i class="demo-psi-view-list"></i>
+    </button>
+    
+    <div class="header-searchbox">
+    
+    <label for="header-search-input" class="header__btn d-md-none btn btn-icon rounded-pill shadow-none border-0 btn-sm" type="button">
+    <i class="demo-psi-magnifi-glass"></i>
+    </label>
+    
+    <form class="searchbox searchbox--auto-expand searchbox--hide-btn input-group">
+    <input id="header-search-input" class="searchbox__input form-control bg-transparent" type="search" placeholder="Type for search . . ." aria-label="Search">
+    <div class="searchbox__backdrop">
+    <button class="searchbox__btn header__btn btn btn-icon rounded shadow-none border-0 btn-sm" type="button" id="button-addon2">
+    <i class="demo-pli-magnifi-glass"></i>
+    </button>
+    </div>
+    </form>
+    </div>
+    </div>
+    
+    
+    <div class="header__content-end">
+    
+    
+    
+    <div class="dropdown">
+    
+    <button class="header__btn btn btn-icon btn-sm" type="button" data-bs-toggle="dropdown" aria-label="Notification dropdown" aria-expanded="false">
+    <span class="d-block position-relative">
+    <i class="demo-psi-bell"></i>
+    <span class="badge badge-super rounded bg-danger p-1">
+    <span class="visually-hidden">unread messages</span>
+    </span>
+    </span>
+    </button>
+    
+    <div class="dropdown-menu dropdown-menu-end w-md-300px">
+    <div class="border-bottom px-3 py-2 mb-3">
+    <h5>Notifications</h5>
+    </div>
+    <div class="list-group list-group-borderless">
+    
+    <div class="list-group-item list-group-item-action d-flex align-items-start mb-3">
+    <div class="flex-shrink-0 me-3">
+    <i class="demo-psi-data-settings text-muted fs-2"></i>
+    </div>
+    <div class="flex-grow-1 ">
+    <a href="#" class="h6 d-block mb-0 stretched-link text-decoration-none">Your storage is full</a>
+    <small class="text-muted">Local storage is nearly full.</small>
+    </div>
+    </div>
+    
+    <div class="list-group-item list-group-item-action d-flex align-items-start mb-3">
+    <div class="flex-shrink-0 me-3">
+    <i class="demo-psi-file-edit text-blue-200 fs-2"></i>
+    </div>
+    <div class="flex-grow-1 ">
+    <a href="#" class="h6 d-block mb-0 stretched-link text-decoration-none">Writing a New Article</a>
+    <small class="text-muted">Wrote a news article for the John Mike</small>
+    </div>
+    </div>
+    
+    <div class="list-group-item list-group-item-action d-flex align-items-start mb-3">
+    <div class="flex-shrink-0 me-3">
+    <i class="demo-psi-speech-bubble-7 text-green-300 fs-2"></i>
+    </div>
+    <div class="flex-grow-1 ">
+    <div class="d-flex justify-content-between align-items-start">
+    <a href="#" class="h6 mb-0 stretched-link text-decoration-none">Comment sorting</a>
+    <span class="badge bg-info rounded ms-auto">NEW</span>
+    </div>
+    <small class="text-muted">You have 1,256 unsorted comments.</small>
+    </div>
+    </div>
+    
+    <div class="list-group-item list-group-item-action d-flex align-items-start mb-3">
+    <div class="flex-shrink-0 me-3">
+    <img class="img-xs rounded-circle" src="assets/img/profile-photos/7.png" alt="Profile Picture" loading="lazy">
+    </div>
+    <div class="flex-grow-1 ">
+    <a href="#" class="h6 d-block mb-0 stretched-link text-decoration-none">Lucy Sent you a message</a>
+    <small class="text-muted">30 minutes ago</small>
+    </div>
+    </div>
+    
+    <div class="list-group-item list-group-item-action d-flex align-items-start mb-3">
+    <div class="flex-shrink-0 me-3">
+    <img class="img-xs rounded-circle" src="assets/img/profile-photos/3.png" alt="Profile Picture" loading="lazy">
+    </div>
+    <div class="flex-grow-1 ">
+    <a href="#" class="h6 d-block mb-0 stretched-link text-decoration-none">Jackson Sent you a message</a>
+    <small class="text-muted">1 hours ago</small>
+    </div>
+    </div>
+    <div class="text-center mb-2">
+    <a href="#" class="btn-link">Show all Notifications</a>
+    </div>
+    </div>
+    </div>
+    </div>
+    
+    
+    <div class="dropdown">
+    
+    <button class="header__btn btn btn-icon btn-sm" type="button" data-bs-toggle="dropdown" aria-label="User dropdown" aria-expanded="false">
+    <i class="demo-psi-male"></i>
+    </button>
+    
+    <div class="dropdown-menu dropdown-menu-end w-md-450px">
+    
+    <div class="d-flex align-items-center border-bottom px-3 py-2">
+    <div class="flex-shrink-0">
+    <img class="img-sm rounded-circle" src="assets/img/profile-photos/1.png" alt="Profile Picture" loading="lazy">
+    </div>
+    <div class="flex-grow-1 ms-3">
+    <h5 class="mb-0">Aaron Chavez</h5>
+    <span class="text-muted fst-italic">aaron_chavez@example.com</span>
+    </div>
+    </div>
+    <div class="row">
+    <div class="col-md-7">
+    
+    <div class="list-group list-group-borderless mb-3">
+    <div class="list-group-item text-center border-bottom mb-3">
+    <p class="h1 display-1 text-green">17</p>
+    <p class="h6 mb-0"><i class="bi bi-cloud fs-3 me-2"></i>CUACA</p>
+
+    <small class="text-muted">You have new orders</small>
+    </div>
+    <div class="list-group-item py-0 d-flex justify-content-between align-items-center">
+    Today Earning
+    <small class="fw-bolder">$578</small>
+    </div>
+    <div class="list-group-item py-0 d-flex justify-content-between align-items-center">
+    Tax
+    <small class="fw-bolder text-danger">- $28</small>
+    </div>
+    <div class="list-group-item py-0 d-flex justify-content-between align-items-center">
+    Total Earning
+    <span class="fw-bold text-primary">$6,578</span>
+    </div>
+    </div>
+    </div>
+    <div class="col-md-5">
+    
+    <div class="list-group list-group-borderless h-100 py-3">
+    <a href="#" class="list-group-item list-group-item-action d-flex justify-content-between align-items-center">
+    <span><i class="demo-pli-mail fs-5 me-2"></i> Messages</span>
+    <span class="badge bg-danger rounded-pill">14</span>
+    </a>
+    <a href="#" class="list-group-item list-group-item-action">
+    <i class="demo-pli-male fs-5 me-2"></i> Profile
+    </a>
+    <a href="#" class="list-group-item list-group-item-action">
+    <i class="demo-pli-gear fs-5 me-2"></i> Settings
+    </a>
+    <a href="#" class="list-group-item list-group-item-action mt-auto">
+    <i class="demo-pli-computer-secure fs-5 me-2"></i> Lock screen
+    </a>
+<a href="#" class="list-group-item list-group-item-action" id="logoutButton" data-toggle="modal" data-target="#exampleModalCenter">
+    <i class="demo-pli-unlock fs-5 me-2"></i> Logout
+</a>
+
+    </div>
+    </div>
+    </div>
+    </div>
+    </div>
+    
+    
+    
+    </div>
+    </div>
+    </div>
+    </header>
